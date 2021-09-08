ca056618a99c51f26556d23ee14ab0c929b8be94
augustoaraujoo <augustinho_darocha1311@hotmail.com>
2021-09-08 11:14:43 -0300

    style * grid

---
+++ client/src/components/buttons/ButtonStyleAside.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Container_Style_Aside">
+    <img
+      id="img_Button"
+      src="https://imagepng.org/wp-content/uploads/2019/08/google-icon-1.png"
+      alt=""
+    />
+    <button id="Button_Style_Aside">
+      {{ txtButton }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    txtButton: Text,
+  },
+};
+</script>
+
+<style>
+.Container_Style_Aside {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+}
+#Button_Style_Aside {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: 1px solid rgb(19, 18, 17);
+  width: 10vw;
+  height: 50px;
+}
+#img_Button {
+  margin-left:auto ;
+  width: auto;
+  height: 30px;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,72 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Home" />
-  </div>
+  <header class="home">
+    <aside class="aside_container">
+      <div class="aside_elements">
+        <ButtonStyleAside txtButton="DashbBoard"/>
+        
+        <ButtonStyleAside txtButton="DashbBoard"/>
+        <ButtonStyleAside txtButton="DashbBoard"/>
+      </div>
+    </aside>
+    <nav>
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est,
+          nesciunt commodi provident explicabo libero ducimus eligendi suscipit
+          itaque aspernatur, aut repellat architecto! Incidunt repudiandae
+          consequatur, tempora aliquam ratione aspernatur dolore?
+        </p>
+      </div>
+    </nav>
+    <footer>
+      <div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
+          a non magnam vel recusandae iusto officia veniam in temporibus. Animi
+          dignissimos eaque, quaerat distinctio aliquid ex error ut non
+          repudiandae?
+        </p>
+      </div>
+    </footer>
+  </header>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import ButtonStyleAside from '../components/buttons/ButtonStyleAside.vue'
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    ButtonStyleAside
   },
 };
 </script>
+<style scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-areas: "aside_container" "nav" "footer";
+  grid-template-columns: 20% 1fr 1fr;
+}
+.aside_container {
+  height: 100vh;
+  background: rgb(245, 237, 237);
+  grid-area: "aside_container";
+}
+.aside_elements{
+  display: grid;
+  flex-direction: column;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+nav {
+  background: rebeccapurple;
+  grid-area: "nav";
+}
+footer {
+  background: purple;
+  grid-area: "footer";
+}
+</style>
